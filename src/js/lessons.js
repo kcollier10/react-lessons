87ad3350a917bad0d2995a5443ad68bd79e061d2
kcollier10 <kristencollier10@gmail.com>
2021-04-11 17:05:59 -0500

    feat: 1/4 of the way through course

--- src/js/lessons.js
+++ src/js/lessons.js
@@ -157,3 +157,12 @@ const calculateSleepDebt = () => {
 }
 calculateSleepDebt();
 // ---------------------------------------
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop();
+secretMessage.push('to', 'Program');
+secretMessage[7] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(6, 5, 'know');
+console.log(secretMessage.slice(',').join(' '));
+// ---------------------------------------
